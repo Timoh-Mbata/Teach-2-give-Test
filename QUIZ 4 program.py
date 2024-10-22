b97fe29d773fc9b61ce442c3193ffb1bd4df0ca3
Timothy Mbata <154816865+Timoh-Mbata@users.noreply.github.com>
2024-10-22 18:40:50 +0300

    Update QUIZ 4 program.py

--- QUIZ 4 program.py
+++ QUIZ 4 program.py
@@ -10,8 +10,8 @@ def reverser(number):
     num = str(number) # takes an
     rev = num[::-1] # string reversed
     if number < 0:
-        abs_num = abs(int(number))  # get the
-        abs_num_str = str(abs_num)
+        abs_num = abs(int(number))  # get the absolute number 
+        abs_num_str = str(abs_num) # convert the number to str to perform reversion
         abs_reversed = abs_num_str[::-1] # reverse the absolute number
         abs_rev = int(abs_reversed)
         if abs_rev > abs_num : # check if the number reversed is greater than the absolute  number
